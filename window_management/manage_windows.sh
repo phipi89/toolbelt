f90cd8ca67f3c67473e60b0afd6d91d3e5ebc1e5
Philipp Aebischer <philipp.aebischer@unibe.ch>
2026-03-06 14:05:50 +0100

    yet another app layout fix

--- window_management/manage_windows.sh
+++ window_management/manage_windows.sh
@@ -2,22 +2,15 @@
 
 export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
+
 APP_NAME="$*"
 CONFIG_PATH="$HOME/toolbelt/config/window_management/setup.yaml"
 
+# 1. Get the action from YAML, fallback to default
 export APP_NAME
+ACTION=$(yq e '.apps[strenv(APP_NAME)].action // .default_action' "$CONFIG_PATH")
+SPAWN_SCRIPT=$(yq e '.apps[strenv(APP_NAME)].spawn_script // ""' "$CONFIG_PATH")
 
-# Support both config styles:
-# apps:
-#   AppName: spawn_new
-# OR
-# apps:
-#   AppName:
-#     action: spawn_new
-ACTION=$(yq e '.apps[strenv(APP_NAME)].action // .apps[strenv(APP_NAME)] // .default_action' "$CONFIG_PATH")
-
-# Optional per-app spawn script (used by spawn_new.sh)
-SPAWN_SCRIPT=$(yq e -r '.apps[strenv(APP_NAME)].spawn_script // ""' "$CONFIG_PATH")
-export SPAWN_SCRIPT
-
-"$HOME/toolbelt/window_management/$ACTION.sh" "$APP_NAME"
+echo "$ACTION"
+echo "$SPAWN_SCRIPT"
+"$HOME/toolbelt/window_management/$ACTION.sh" "$APP_NAME" "$SPAWN_SCRIPT"
