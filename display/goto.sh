530f7ce0f00cfee32d55b224be32ab93df251e80
Philipp Aebischer <philipp.aebischer@unibe.ch>
2026-03-25 14:15:59 +0100

    small fixes in run in combination with display layout

--- display/goto.sh
+++ display/goto.sh
@@ -6,10 +6,20 @@ hs -c "
 local appName = '$APP_NAME'
 local app = hs.application.get(appName)
 
+local function safeCall(obj, methodName)
+    if not obj then return nil end
+    local method = obj[methodName]
+    if type(method) ~= 'function' then return nil end
+    local ok, result = pcall(method, obj)
+    if ok then return result end
+    return nil
+end
+
 -- 1. Try to find any standard window belonging to the app
 local win = nil
 if app then
-    win = app:mainWindow() or app:allWindows()[1]
+    local windows = safeCall(app, 'allWindows') or {}
+    win = safeCall(app, 'focusedWindow') or safeCall(app, 'mainWindow') or windows[1]
 end
 
 if win then
