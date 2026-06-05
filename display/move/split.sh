f349f90cfa10967e18d821c3fc51d52c7f6975c6
Philipp Aebischer <philipp.aebischer@unibe.ch>
2026-06-05 11:05:20 +0200

    horizontal split cycles to 50/50 <> 2/1

--- display/move/split.sh
+++ display/move/split.sh
@@ -9,10 +9,8 @@ case "$1" in
     ;;
 esac
 
-export FRONTMOST_RIGHT="$frontmost_right"
-
 hs -c "
-local frontmostRight = os.getenv('FRONTMOST_RIGHT') == 'true'
+local frontmostRight = ${frontmost_right}
 local focus = hs.window.focusedWindow()
 local screen = (focus and focus:screen()) or hs.mouse.getCurrentScreen()
 if not screen then return end
@@ -28,14 +26,45 @@ end
 if #wins < 2 then return end
 
 local sf = screen:frame()
-local left = hs.geometry.rect(sf.x, sf.y, sf.w / 2, sf.h)
-local right = hs.geometry.rect(sf.x + sf.w / 2, sf.y, sf.w / 2, sf.h)
+local tol = 8
+local leftHalf = hs.geometry.rect(sf.x, sf.y, sf.w / 2, sf.h)
+local rightHalf = hs.geometry.rect(sf.x + sf.w / 2, sf.y, sf.w / 2, sf.h)
+local leftTwoThirds = hs.geometry.rect(sf.x, sf.y, sf.w * 2 / 3, sf.h)
+local rightOneThird = hs.geometry.rect(sf.x + sf.w * 2 / 3, sf.y, sf.w / 3, sf.h)
+local leftOneThird = hs.geometry.rect(sf.x, sf.y, sf.w / 3, sf.h)
+local rightTwoThirds = hs.geometry.rect(sf.x + sf.w / 3, sf.y, sf.w * 2 / 3, sf.h)
 
+local function close(a, b) return math.abs(a - b) <= tol end
+local function same(a, b)
+  return close(a.x, b.x) and close(a.y, b.y) and close(a.w, b.w) and close(a.h, b.h)
+end
+
+local leftWin, rightWin = wins[1], wins[2]
 if frontmostRight then
-  wins[1]:setFrame(right, 0)
-  wins[2]:setFrame(left, 0)
+  leftWin, rightWin = wins[2], wins[1]
+end
+
+local isHalf = same(leftWin:frame(), leftHalf) and same(rightWin:frame(), rightHalf)
+local isWeighted = nil
+if frontmostRight then
+  isWeighted = same(leftWin:frame(), leftOneThird) and same(rightWin:frame(), rightTwoThirds)
 else
-  wins[1]:setFrame(left, 0)
-  wins[2]:setFrame(right, 0)
+  isWeighted = same(leftWin:frame(), leftTwoThirds) and same(rightWin:frame(), rightOneThird)
+end
+
+if isHalf then
+  if frontmostRight then
+    leftWin:setFrame(leftOneThird, 0)
+    rightWin:setFrame(rightTwoThirds, 0)
+  else
+    leftWin:setFrame(leftTwoThirds, 0)
+    rightWin:setFrame(rightOneThird, 0)
+  end
+elseif isWeighted then
+  leftWin:setFrame(leftHalf, 0)
+  rightWin:setFrame(rightHalf, 0)
+else
+  leftWin:setFrame(leftHalf, 0)
+  rightWin:setFrame(rightHalf, 0)
 end
 "
