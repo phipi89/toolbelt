195e33acf6d3f603e92526738bab261ae8768146
Philipp Aebischer <philipp.aebischer@unibe.ch>
2026-07-13 09:52:54 +0200

    cleanup

--- aliases.sh
+++ aliases.sh
@@ -1,42 +1,21 @@
-#                                  _
-#   __ _  ___ _ __   ___ _ __ __ _| |
-#  / _` |/ _ \ '_ \ / _ \ '__/ _` | |
-# | (_| |  __/ | | |  __/ | | (_| | |
-#  \__, |\___|_| |_|\___|_|  \__,_|_|
-#  |___/
+# GENERAL
 
 TOOLBELT="$HOME/toolbelt"
 source ~/toolbelt/auth.sh
 
 
-#
-#  _ __ _   _ _ __
-# | '__| | | | '_ \
-# | |  | |_| | | | |
-# |_|   \__,_|_| |_|
-#
+# ESSENTIALS
 
 run() { (cd "$TOOLBELT/run" || return 1; uv run run;) }
+calc() { (cd "$TOOLBELT/calc" || return 1; uv run calc;) }
+snippets() { (cd "$TOOLBELT/snippets" || return 1; uv run cli "$@";) }
+buf() { "$TOOLBELT/buffer/open.sh" "$@"; }
 
 
-#  ____       _____
-# |  _ \ _   |_   _|__ _ __ ___  _ __
-# | |_) | | | || |/ _ \ '_ ` _ \| '_ \
-# |  __/| |_| || |  __/ | | | | | |_) |
-# |_|    \__, ||_|\___|_| |_| |_| .__/
-#        |___/                  |_|
-
-source "$TOOLBELT/pytemp/pytemp.sh"
-
-
-#                          _
-#  ___  ___  __ _ _ __ ___| |__
-# / __|/ _ \/ _` | '__/ __| '_ \
-# \__ \  __/ (_| | | | (__| | | |
-# |___/\___|\__,_|_|  \___|_| |_|
-#
+# SEARCH
 
 search() { (cd "$TOOLBELT/search" || return 1; clear; uv run search "$@";) }
+
 goto() {
   local tmp target exit_status
   tmp="$(mktemp)" || return 1
@@ -52,17 +31,16 @@ goto() {
 }
 
 
+# PYTHON
 
-#       _ _
-#   ___| | | __
-#  / __| | |/ /
-# | (__| |   <
-#  \___|_|_|\_\
+source "$TOOLBELT/pytemp/pytemp.sh"
+
+
+# TIME TRACKING
 
 # wrap in () runs the command in a subshell
 _clk() { (cd "$TOOLBELT/clk"; uv run clk "$@";) }
 alias clk='noglob _clk'
-
 
 
 # TRANSLATION
@@ -76,39 +54,32 @@ tit() { (cd "$TOOLBELT/translate" || return 1; uv run translate tit "$@";) }
 fit() { (cd "$TOOLBELT/translate" || return 1; uv run translate fit "$@";) }
 
 
-
-#            _
-#  _ __ ___ (_)___  ___
-# | '_ ` _ \| / __|/ __|
-# | | | | | | \__ \ (__
-# |_| |_| |_|_|___/\___|
-#
-
-alias textitle='uv run --no-project --with pyperclip,pyfiglet $TOOLBELT/misc/textitle.py'
-alias pytitle='uv run --no-project --with pyperclip,pyfiglet $TOOLBELT/misc/textitle.py --prefix "#"'
-
-ask(){ opencode run "$*"; }
+## FINDER INTERACTION
 
 source "$TOOLBELT/grab/grab.sh"
+
+gtouch() {
+  grab || return 1
+  if [[ $# -eq 0 ]]; then
+    touch empty.txt
+  else
+    touch "$@"
+  fi
+}
+
+
+# LLMS
+
+alias oc="opencode -c"
+ask(){ opencode run "$*"; }
+
+
+# MISCELANEOUS
+timer() { clear && "$TOOLBELT/misc/timer.py"; }
+alias textitle='uv run --no-project --with pyperclip,pyfiglet $TOOLBELT/misc/textitle.py'
+alias pytitle='uv run --no-project --with pyperclip,pyfiglet $TOOLBELT/misc/textitle.py --prefix "#"'
 source "$TOOLBELT/misc/compose_mail.sh"
 source "$TOOLBELT/misc/yank-to-clipboard.sh"
-snippets() { (cd "$TOOLBELT/snippets" || return 1; uv run cli "$@";) }
-
 source $TOOLBELT/misc/diskusage.sh
 source $TOOLBELT/misc/eject-all.sh
-alias oc="opencode -c"
-
-
-
-#            _
-#   ___ __ _| | ___
-#  / __/ _` | |/ __|
-# | (_| (_| | | (__
-#  \___\__,_|_|\___|
-#
-
-calc() { (cd "$TOOLBELT/calc" || return 1; uv run calc;) }
-
-timer() { clear && "$TOOLBELT/misc/timer.py"; }
-
-buf() { "$TOOLBELT/buffer/open.sh" "$@"; }
+setup() { "$TOOLBELT/setup/setup.sh" "$@"; }
